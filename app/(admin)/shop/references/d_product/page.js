09638cc93e7ddd352410b4d973780f42bf47c886
Roman <didoshakr@ukr.net>
2024-01-05 16:19:00 +0200

    2

--- app/(admin)/shop/references/d_product/page.js
+++ app/(admin)/shop/references/d_product/page.js
@@ -5,7 +5,7 @@ export default async function DProducts() {
   // "COALESCE(to_char(date_create, 'MM-DD-YYYY'), '') AS datecreate,"-перетворення date в to_char
   // LIMIT 100"
   let resData =
-    await sql`SELECT d_product.id,d_product.name,category_id,price,brand_id,img,ov_id,skod,uktzed,pdv,akcuz,is_discount,date_create,COALESCE(to_char(date_create, 'YYYY-MM-DD'), '') AS datecreate,d_category.name AS category,d_brand.name AS brand,d_ov.name AS ov FROM d_product JOIN d_category ON d_category.id = d_product.category_id   JOIN d_brand ON d_brand.id = d_product.brand_id JOIN d_ov ON d_ov.kod = d_product.ov_id  ORDER BY id LIMIT 20`
+    await sql`SELECT d_product.id,d_product.name,category_id,price,brand_id,img,ov_id,skod,uktzed,pdv,akcuz,is_discount,date_create,COALESCE(to_char(date_create, 'YYYY-MM-DD'), '') AS datecreate,d_category.name AS category,d_brand.name AS brand,d_ov.name AS ov FROM d_product JOIN d_category ON d_category.id = d_product.category_id   JOIN d_brand ON d_brand.id = d_product.brand_id JOIN d_ov ON d_ov.kod = d_product.ov_id  ORDER BY id`
 
   //   let resData = []
   //   const query =
